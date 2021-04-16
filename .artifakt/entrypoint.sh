a5de53af9ee637fddb50e1ab7c62e62702723048
Steven Bressey <steven.bressey@mediakeys.com>
2021-04-16 14:06:04 +0200

    use APP_LOG_DIR env var for artifakt log path

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -8,6 +8,15 @@ env
 
 APP_ENV=dev composer --no-ansi --no-interaction install --no-cache --no-progress  --no-autoloader --no-scripts
 
+# Temp define APP_LOG_DIR variable while waiting for console env variables to be working
+export APP_LOG_DIR=/var/log/artifakt
+
+#allow www-data user to write in artifakt logs folder
+if [[ -n ${APP_LOG_DIR} && -d ${APP_LOG_DIR} ]]; then
+  echo "updating ${APP_LOG_DIR} folder permissions"
+  chown www-data:www-data -R "${APP_LOG_DIR}"
+fi
+
 # wait for database to be ready
 export ARTIFAKT_MYSQL_HOST=mysql
 /.artifakt/wait-for-it.sh $ARTIFAKT_MYSQL_HOST:3306
@@ -21,11 +30,5 @@ php bin/console cache:clear --env=prod && composer dump-autoload
 # force fixtures (temporary force APP_ENV=dev even if it is set to "prod")
 # https://miary.dev/2020/12/20/symfony-doctrinefixturesbundle-installe-doctrinefixturesload-non-trouve/
 APP_ENV=dev php bin/console doctrine:fixtures:load --no-ansi --no-interaction
-
-#allow www-data user to write in artifakt logs folder
-if [ -d "/var/log/artifakt" ] ; then
-  echo "updating artifakt logs folder owner"
-  chown www-data:www-data -R /var/log/artifakt
-fi
 
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
