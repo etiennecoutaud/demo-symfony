6fff8cbcc168a8b3cadc17342fc5f7932da8c4e4
Steven Bressey <steven.bressey@mediakeys.com>
2021-04-23 15:00:34 +0200

    move logs & data perms commands at top of entrypoint

--- .artifakt/entrypoint.sh
+++ .artifakt/entrypoint.sh
@@ -6,6 +6,13 @@ echo ">>>>>>>>>>>>>> START CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
 # debug env vars.
 env
 
+# enable catchall folder, don't forget to set APP_LOG_DIR to /var/log/artifakt
+mkdir -p /var/log/artifakt && chown -R www-data:www-data /var/log/artifakt
+
+# set correct permissions on /data folder and create symlinks
+chown -R www-data:www-data /data
+ln -s /data/uploads /var/www/html/public/uploads
+
 APP_ENV=dev composer --no-ansi --no-interaction install --no-cache --no-progress  --no-autoloader --no-scripts
 
 # wait for database to be ready
@@ -20,13 +27,5 @@ php bin/console cache:clear --env=prod && composer dump-autoload
 # force fixtures (temporary force APP_ENV=dev even if it is set to "prod")
 # https://miary.dev/2020/12/20/symfony-doctrinefixturesbundle-installe-doctrinefixturesload-non-trouve/
 APP_ENV=dev php bin/console doctrine:fixtures:load --no-ansi --no-interaction
-
-# enable catchall folder, don't forget to set APP_LOG_DIR to /var/log/artifakt
-mkdir -p /var/log/artifakt && chown -R www-data:www-data /var/log/artifakt
-
-# set correct permissions on /data folder and create symlinks
-chown -R www-data:www-data /data
-ln -s /data/uploads /var/www/html/public/uploads
-
 
 echo ">>>>>>>>>>>>>> END CUSTOM ENTRYPOINT SCRIPT <<<<<<<<<<<<<<<<< "
